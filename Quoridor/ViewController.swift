57c1f16bba8ad04f898e44a596cdb242d0407290
Artur Vypovskyi <arturvypovskyy@gmail.com>
2021-10-06 17:35:58 +0300

    Initial Commit

---
+++ Quoridor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quoridor
+//
+//  Created by Артур on 06.10.2021.
+//  Copyright © 2021 Artur Vypovskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
